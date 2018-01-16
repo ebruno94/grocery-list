8e8f63edc1eca11fd710b32fb0d2b5d3f3f3cf44
Drew Tucker and Ernest Bruno <ernest@>
2018-01-16 13:24:05 -0800

    Add Javascript that allows user to put items in a list, and sorts the list with a click of a button

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  var items = [];
+  var upperItems;
+
+
+  $("#formItem").submit(function(event){
+    var currItem = $("#items").val();
+    items.push(currItem);
+    $("#soFar").append("<li>" + currItem + "</li>");
+    $("document")
+
+    event.preventDefault();
+  });
+  $("#sort").click(function(event){
+    $("#soFar").text("");
+    $("#sorted").text("");
+    upperItems = items.map(function(item){
+      return item.toUpperCase();
+    });
+    upperItems.sort();
+    upperItems.forEach(function(upperItem){
+      $("#sorted").append("<li>"+ upperItem + "</li>");
+    });
+    event.preventDefault();
+  });
+});
